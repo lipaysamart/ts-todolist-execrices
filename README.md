2037a868902a03cce01a19ba730b278c5a1e7575
lipaysam <lipaysamart@gmail.com>
2025-04-09 00:12:19 -0700

    first commit: 开发环境搭建

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ts-todolist-execrices
-使用 TypeScript + Vue3 开发的待办事项应用练习。包含添加、编辑、刪除任务等功能，通过练习来熟悉组件、响应式数据、事件处理、状态管理。
+
+## Technology
+- TypeScript
+- Vue3
+- tailwindcss
+
+## Goat
+- 熟悉组件、响应式数据、事件处理、状态管理。
+
+## Effect
+- 实现添加、编辑、刪除任务等功能。
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="wrapped">
+    <HelloWorld />
+    <p>This is Todo List App.</p>
+  </div>
+</template>
+
+<style scoped></style>
